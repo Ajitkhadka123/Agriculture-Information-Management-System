8642da1227042dec0244b6dbd75478d720174127
Ajitkhadka123 <101698296+Ajitkhadka123@users.noreply.github.com>
2022-04-20 20:13:25 +0545

    Update and rename MainPage.java to AdminDashboard.java
    Button and color changes

--- AIMS/SignUp/src/AdminDashboard.java
+++ AIMS/SignUp/src/AdminDashboard.java
@@ -8,12 +8,16 @@ import javax.swing.JOptionPane;
 
 import java.awt.Font;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.border.LineBorder;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 
-public class MainPage {
+public class AdminDashboard {
 
 	JFrame frame;
 
@@ -24,7 +28,7 @@ public class MainPage {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MainPage window = new MainPage();
+					AdminDashboard window = new AdminDashboard();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -36,7 +40,7 @@ public class MainPage {
 	/**
 	 * Create the application.
 	 */
-	public MainPage() {
+	public AdminDashboard() {
 		initialize();
 	}
 
@@ -45,33 +49,53 @@ public class MainPage {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.getContentPane().setBackground(new Color(255, 255, 153));
+		frame.getContentPane().setBackground(new Color(205,92,92));
 		frame.setBounds(100, 100, 654, 442);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
 		JButton btnLogOut = new JButton("Log Out");
+		btnLogOut.addMouseListener(new MouseAdapter() {
+			@Override
+			
+				public void mouseEntered(MouseEvent e) {
+				btnLogOut.setForeground(Color.RED);
+				}
+				public void mouseExited(MouseEvent e) {
+					btnLogOut.setForeground(Color.BLACK);
+				}
+			@Override
+			    public void mouseClicked(MouseEvent e) {
+				
+				
+			}
+			});
+			
 		btnLogOut.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int answer = JOptionPane.showConfirmDialog(frame, "Do you want to log out", "Back To Log in Page",
+				int answer = JOptionPane.showConfirmDialog(frame, "Do you want to log out?", "Back To Log in Page",
 						JOptionPane.YES_NO_OPTION);
 
 				if (answer == JOptionPane.NO_OPTION) {
 					// do something
 				} else if (answer == JOptionPane.YES_OPTION) {
-					System.exit(0);
+					frame.setVisible(false);
+					MainLogin Login= new MainLogin();
+					Login.setVisible(true);
+					
+					
 				}
 				
 			}
 		});
 		btnLogOut.setFont(new Font("Triumvirate Cond", Font.BOLD, 14));
-		btnLogOut.setBounds(545, 10, 85, 33);
+		btnLogOut.setBounds(530, 10, 100, 33);
 		frame.getContentPane().add(btnLogOut);
 
 		JPanel panel = new JPanel();
-		panel.setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		panel.setBackground(new Color(255, 255, 51));
-		panel.setBounds(127, 122, 304, 177);
+		panel.setBorder(new LineBorder(new Color(255, 182, 193), 2));
+		panel.setBackground(new Color(205,92,92));
+		panel.setBounds(168, 111, 304, 177);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 
@@ -92,7 +116,8 @@ public class MainPage {
 		panel.add(btnCreate);
 
 		JLabel lblNewLabel = new JLabel("Things Admin can do");
-		lblNewLabel.setBounds(79, 10, 164, 27);
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setBounds(62, 10, 187, 27);
 		panel.add(lblNewLabel);
 		lblNewLabel.setFont(new Font("Triumvirate Cond", Font.BOLD, 17));
 
