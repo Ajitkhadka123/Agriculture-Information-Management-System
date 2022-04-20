a5f5adf3b78ec2d964edb1b8cf7a000b3f21e21f
Ajitkhadka123 <101698296+Ajitkhadka123@users.noreply.github.com>
2022-04-20 20:15:30 +0545

    Update CreatePage.java
    color and button changes

--- AIMS/SignUp/src/CreatePage.java
+++ AIMS/SignUp/src/CreatePage.java
@@ -1,28 +1,24 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JButton;
 import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
-import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class CreatePage {
+public class Delete {
 
 	JFrame frame;
-	private JTextField NametextField;
-	private JTextField usernametextField;
-	private JTextField passwordtextField;
-	private JTextField mailtextField;
-	private JTextField gendertextField;
-	private JTextField dobtextField;
-	private JTextField locationtextField;
+	private JTextField nametextField;
 	private JTextField IdtextField;
+	private JTextField usertextField;
 
+	
 	/**
 	 * Launch the application.
 	 */
@@ -30,7 +26,7 @@ public class CreatePage {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					CreatePage window = new CreatePage();
+					Delete window = new Delete();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -42,7 +38,7 @@ public class CreatePage {
 	/**
 	 * Create the application.
 	 */
-	public CreatePage() {
+	public Delete() {
 		initialize();
 	}
 
@@ -51,16 +47,16 @@ public class CreatePage {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.getContentPane().setBackground(new Color(255, 255, 153));
-		frame.setBounds(100, 100, 587, 393);
+		frame.getContentPane().setBackground(new Color(205,92,92));
+		frame.setBounds(100, 100, 626, 397);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-
+		
 		JButton btnBack = new JButton("Back");
 		btnBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
-					MainPage main = new MainPage();
+					AdminDashboard main = new AdminDashboard();
 					main.frame.setVisible(true);
 					frame.setVisible(false);
 				} catch (Exception e2) {
@@ -69,130 +65,68 @@ public class CreatePage {
 			}
 		});
 		btnBack.setFont(new Font("Triumvirate Cond", Font.BOLD, 15));
-		btnBack.setBounds(478, 10, 85, 33);
+		btnBack.setBounds(517, 10, 85, 33);
 		frame.getContentPane().add(btnBack);
-
-		JButton btnCreateUser = new JButton("Create User");
-		btnCreateUser.addActionListener(new ActionListener() {
+		
+		nametextField = new JTextField();
+		nametextField.setColumns(10);
+		nametextField.setBounds(203, 146, 183, 25);
+		frame.getContentPane().add(nametextField);
+		
+		JLabel lblNewLabel = new JLabel("Name");
+		lblNewLabel.setFont(new Font("Triumvirate Cond", Font.BOLD, 15));
+		lblNewLabel.setBounds(203, 126, 45, 13);
+		frame.getContentPane().add(lblNewLabel);
+		
+		IdtextField = new JTextField();
+		IdtextField.setColumns(10);
+		IdtextField.setBounds(203, 91, 183, 25);
+		frame.getContentPane().add(IdtextField);
+		
+		JLabel lblUserId = new JLabel("User Id");
+		lblUserId.setFont(new Font("Triumvirate Cond", Font.BOLD, 15));
+		lblUserId.setBounds(203, 71, 85, 13);
+		frame.getContentPane().add(lblUserId);
+		
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setFont(new Font("Triumvirate Cond", Font.BOLD, 15));
+		lblUsername.setBounds(203, 185, 85, 13);
+		frame.getContentPane().add(lblUsername);
+		
+		usertextField = new JTextField();
+		usertextField.setColumns(10);
+		usertextField.setBounds(203, 205, 183, 25);
+		frame.getContentPane().add(usertextField);
+		
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
 					int UserId = Integer.parseInt(IdtextField.getText());
-					String Name = NametextField.getText();
-					String Username = usernametextField.getText();
-					String Password = passwordtextField.getText();
-					String Email = mailtextField.getText();
-					String Gender = gendertextField.getText();
-					String Location = locationtextField.getText();
-					String DoB = dobtextField.getText();
-					System.out.println("User ID: " + UserId + "\nName: " + Name + "\nUsername: " + Username
-							+ "\nPassword: " + Password + "\nEmail: " + Email + "\nGender: " + Gender + "\nLocation: "
-							+Location+"\nDoB: "+DoB);
-					JOptionPane.showMessageDialog(frame,"User created");
-
+					String Name = nametextField.getText();
+					String Username = usertextField.getText();
+					System.out.println("Deleting user with: \nUserId: "+ UserId + "\nName: " + Name + "\nUsername: " + Username);
+					JOptionPane.showMessageDialog(frame,"User Deleted");
 				} catch (Exception e2) {
 					System.out.println(e2);
 				}
 			}
 		});
-		btnCreateUser.setFont(new Font("Triumvirate Cond", Font.BOLD, 15));
-		btnCreateUser.setBounds(113, 300, 107, 44);
-		frame.getContentPane().add(btnCreateUser);
-
+		btnDelete.setFont(new Font("Triumvirate Cond", Font.BOLD, 15));
+		btnDelete.setBounds(191, 269, 85, 33);
+		frame.getContentPane().add(btnDelete);
+		
 		JButton btnClear = new JButton("Clear");
 		btnClear.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				IdtextField.setText("");
-				NametextField.setText("");
-				usernametextField.setText("");
-				passwordtextField.setText("");
-				mailtextField.setText("");
-				gendertextField.setText("");
-				locationtextField.setText("");
-				dobtextField.setText("");
+				nametextField.setText("");
+				usertextField.setText("");
 			}
 		});
 		btnClear.setFont(new Font("Triumvirate Cond", Font.BOLD, 15));
-		btnClear.setBounds(384, 300, 85, 44);
+		btnClear.setBounds(316, 269, 85, 33);
 		frame.getContentPane().add(btnClear);
-
-		NametextField = new JTextField();
-		NametextField.setBounds(81, 132, 183, 25);
-		frame.getContentPane().add(NametextField);
-		NametextField.setColumns(10);
-
-		JLabel lblNewLabel = new JLabel("Name");
-		lblNewLabel.setFont(new Font("Triumvirate Cond", Font.BOLD, 15));
-		lblNewLabel.setBounds(81, 112, 45, 13);
-		frame.getContentPane().add(lblNewLabel);
-
-		JLabel lblUsername = new JLabel("Username");
-		lblUsername.setFont(new Font("Triumvirate Cond", Font.BOLD, 15));
-		lblUsername.setBounds(81, 171, 85, 13);
-		frame.getContentPane().add(lblUsername);
-
-		usernametextField = new JTextField();
-		usernametextField.setColumns(10);
-		usernametextField.setBounds(81, 191, 183, 25);
-		frame.getContentPane().add(usernametextField);
-
-		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setFont(new Font("Triumvirate Cond", Font.BOLD, 15));
-		lblPassword.setBounds(81, 226, 85, 13);
-		frame.getContentPane().add(lblPassword);
-
-		passwordtextField = new JTextField();
-		passwordtextField.setColumns(10);
-		passwordtextField.setBounds(81, 246, 183, 25);
-		frame.getContentPane().add(passwordtextField);
-
-		JLabel lblEmail = new JLabel("Email");
-		lblEmail.setFont(new Font("Triumvirate Cond", Font.BOLD, 15));
-		lblEmail.setBounds(322, 57, 45, 13);
-		frame.getContentPane().add(lblEmail);
-
-		mailtextField = new JTextField();
-		mailtextField.setColumns(10);
-		mailtextField.setBounds(322, 77, 183, 25);
-		frame.getContentPane().add(mailtextField);
-
-		JLabel lblGedner = new JLabel("Gender");
-		lblGedner.setFont(new Font("Triumvirate Cond", Font.BOLD, 15));
-		lblGedner.setBounds(322, 112, 45, 13);
-		frame.getContentPane().add(lblGedner);
-
-		gendertextField = new JTextField();
-		gendertextField.setColumns(10);
-		gendertextField.setBounds(322, 132, 183, 25);
-		frame.getContentPane().add(gendertextField);
-
-		JLabel lblDob = new JLabel("DOB");
-		lblDob.setFont(new Font("Triumvirate Cond", Font.BOLD, 15));
-		lblDob.setBounds(322, 226, 45, 13);
-		frame.getContentPane().add(lblDob);
-
-		dobtextField = new JTextField();
-		dobtextField.setColumns(10);
-		dobtextField.setBounds(322, 246, 183, 25);
-		frame.getContentPane().add(dobtextField);
-
-		JLabel lblLocation = new JLabel("Location");
-		lblLocation.setFont(new Font("Triumvirate Cond", Font.BOLD, 15));
-		lblLocation.setBounds(322, 171, 67, 13);
-		frame.getContentPane().add(lblLocation);
-
-		locationtextField = new JTextField();
-		locationtextField.setColumns(10);
-		locationtextField.setBounds(322, 191, 183, 25);
-		frame.getContentPane().add(locationtextField);
-
-		JLabel lblUserId = new JLabel("User Id");
-		lblUserId.setFont(new Font("Triumvirate Cond", Font.BOLD, 15));
-		lblUserId.setBounds(81, 57, 45, 13);
-		frame.getContentPane().add(lblUserId);
-
-		IdtextField = new JTextField();
-		IdtextField.setColumns(10);
-		IdtextField.setBounds(81, 77, 183, 25);
-		frame.getContentPane().add(IdtextField);
 	}
+
 }
