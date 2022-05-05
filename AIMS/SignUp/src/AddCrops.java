013ac8c934907b554927ec9fd3f778e9ba5ce547
Ajitkhadka123 <101698296+Ajitkhadka123@users.noreply.github.com>
2022-05-06 00:59:25 +0545

    Update AddCrops.java

--- AIMS/SignUp/src/AddCrops.java
+++ AIMS/SignUp/src/AddCrops.java
@@ -10,14 +10,18 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.Image;
+
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Random;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
 import javax.swing.Box;
 import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
@@ -25,17 +29,34 @@ import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
 
 public class AddCrops extends JFrame {
 
 	private JPanel contentPane;
 	private JFrame frame;
-	private JTextField CropTextfield;
+	private JTextField name;
 	private JTextField FarmerR;
 	private JTextField MarketB;
 	private JTextField Back;
 	DefaultTableModel model;
 	private JTable table;
+	private JTextField crop;
+	private JTextField Quantity;
+	private JTextField id;
+	private JTextField noofbox;
+	long first4;
+	long first;
+	
+	private Image logo = new ImageIcon(Login.class.getResource("/img/corpslogo.png")).getImage().getScaledInstance(50,50, Image.SCALE_SMOOTH);
+	
+	public void Random(){
+	    Random ran = new Random();
+	    first4 = (ran.nextLong() % 9000L) + 1000L;
+	    first = Math.abs(first4); // abs for on positive numbers
+
+		}
+	
 	/**
 	 * Launch the application.
 	 */
@@ -57,11 +78,12 @@ public class AddCrops extends JFrame {
 	 */
 	public AddCrops() {
 		setResizable(false);
+		Random();
 		
 		//creating methods for lables and textfields
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1400, 700);
+		setBounds(100, 0, 1406, 818);
 		//setUndecorated(true);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(85, 107, 47));
@@ -69,22 +91,22 @@ public class AddCrops extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblCrops = new JLabel("Production");
-		lblCrops.setFont(new Font("Calibri", Font.PLAIN, 20));
-		lblCrops.setForeground(Color.WHITE);
-		lblCrops.setBounds(271, 182, 144, 25);
-		contentPane.add(lblCrops);
+		JLabel QuantityL = new JLabel("Quantity");
+		QuantityL.setFont(new Font("Calibri", Font.PLAIN, 20));
+		QuantityL.setForeground(Color.WHITE);
+		QuantityL.setBounds(253, 311, 144, 25);
+		contentPane.add(QuantityL);
 		
 		JLabel lblMarketRate = new JLabel("Market Rate");
 		lblMarketRate.setForeground(Color.WHITE);
 		lblMarketRate.setFont(new Font("Calibri", Font.PLAIN, 20));
-		lblMarketRate.setBounds(688, 182, 110, 25);
+		lblMarketRate.setBounds(688, 311, 110, 25);
 		contentPane.add(lblMarketRate);
 		
 		JLabel lblFarmersRate = new JLabel("Farmers Rate");
 		lblFarmersRate.setForeground(Color.WHITE);
 		lblFarmersRate.setFont(new Font("Calibri", Font.PLAIN, 20));
-		lblFarmersRate.setBounds(478, 182, 144, 25);
+		lblFarmersRate.setBounds(688, 180, 144, 25);
 		contentPane.add(lblFarmersRate);
 		
 		JButton PrintButton = new JButton("Print");             //creating save button
@@ -116,25 +138,25 @@ public class AddCrops extends JFrame {
 		JLabel lblCrops_1 = new JLabel("Crops");
 		lblCrops_1.setForeground(Color.WHITE);
 		lblCrops_1.setFont(new Font("Calibri", Font.PLAIN, 20));
-		lblCrops_1.setBounds(38, 182, 71, 25);
+		lblCrops_1.setBounds(38, 311, 71, 25);
 		contentPane.add(lblCrops_1);
 		
 		FarmerR = new JTextField();
 		FarmerR.setFont(new Font("Arial", Font.PLAIN, 16));
 		FarmerR.setColumns(10);
-		FarmerR.setBounds(478, 217, 175, 31);
+		FarmerR.setBounds(688, 217, 175, 31);
 		contentPane.add(FarmerR);
 		
 		MarketB = new JTextField();
 		MarketB.setFont(new Font("Arial", Font.PLAIN, 16));
 		MarketB.setColumns(10);
-		MarketB.setBounds(688, 217, 175, 31);
+		MarketB.setBounds(688, 345, 175, 31);
 		contentPane.add(MarketB);
 		
 		JComboBox comboBoxP = new JComboBox();
 		comboBoxP.setModel(new DefaultComboBoxModel(new String[] {"Kilogram ", "Gram", "Miligram", "Pound", "Ton"}));
 		comboBoxP.setFont(new Font("Arial", Font.PLAIN, 16));
-		comboBoxP.setBounds(271, 218, 177, 29);
+		comboBoxP.setBounds(473, 346, 177, 29);
 		contentPane.add(comboBoxP);
 		
 		JLabel lblAddCropSection = new JLabel("Add Crop Section");
@@ -150,15 +172,15 @@ public class AddCrops extends JFrame {
 		contentPane.add(verticalBox);
 
 		
-		CropTextfield = new JTextField();
-		CropTextfield.setFont(new Font("Arial", Font.PLAIN, 16));
-		CropTextfield.setBounds(38, 220, 175, 29);
-		contentPane.add(CropTextfield);
-		CropTextfield.setColumns(10);
+		name = new JTextField();
+		name.setFont(new Font("Arial", Font.PLAIN, 16));
+		name.setBounds(253, 218, 175, 29);
+		contentPane.add(name);
+		name.setColumns(10);
 		
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(38, 335, 825, 318);
+		scrollPane.setBounds(38, 453, 825, 318);
 		contentPane.add(scrollPane);
 		
 		table = new JTable();
@@ -166,17 +188,21 @@ public class AddCrops extends JFrame {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				int i = table.getSelectedRow();
-				CropTextfield.setText(model.getValueAt(i, 0).toString());
-    			comboBoxP.setSelectedItem(model.getValueAt(i, 1).toString());
-				FarmerR.setText(model.getValueAt(i, 2).toString());
-				MarketB.setText(model.getValueAt(i, 3).toString());
+				id.setText(model.getValueAt(i, 0).toString());
+				name.setText(model.getValueAt(i, 1).toString());
+				noofbox.setText(model.getValueAt(i, 2).toString());
+				crop.setText(model.getValueAt(i, 3).toString());
+				Quantity.setText(model.getValueAt(i, 4).toString());
+    			comboBoxP.setSelectedItem(model.getValueAt(i, 5).toString());
+				FarmerR.setText(model.getValueAt(i, 6).toString());
+				MarketB.setText(model.getValueAt(i, 7).toString());
 				
 				
 			}
 		});
 		model = new DefaultTableModel();
-		Object[] column = {"Crops","Production","Farmers Rate","Market rate"};
-		final Object[] row = new Object[4];
+		Object[] column = {"ID","Name","No. of Family","Crops","Quantity","Production","Farmers Rate","Market rate"};
+		final Object[] row = new Object[8];
 		model.setColumnIdentifiers(column);
 		table.setModel(model);
 		scrollPane.setViewportView(table);
@@ -188,23 +214,32 @@ public class AddCrops extends JFrame {
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
-				row[0]= CropTextfield.getText();
-				row[1]= comboBoxP.getSelectedItem();
-				row[2]= FarmerR.getText();
-				row[3]= MarketB.getText();
+				row[0]= id.getText();
+				row[1]= name.getText();
+				row[2]= noofbox.getText();
+				row[3]= crop.getText();
+				row[4]= Quantity.getText();
+				row[5]= comboBoxP.getSelectedItem();
+				row[6]= FarmerR.getText();
+				row[7]= MarketB.getText();
 				model.addRow(row);
 				
- 				comboBoxP.setSelectedItem("");
-				CropTextfield.setText("");
+ 				id.setText("");
+				name.setText("");
+				noofbox.setText("");
+				crop.setText("");
+				Quantity.setText("");
+				comboBoxP.setSelectedItem("");
 				FarmerR.setText("");
 				MarketB.setText("");
 				JOptionPane.showMessageDialog(null,"Saved");
 				}catch(Exception E) {
 					System.out.println(E);
 				}
+				Random();
 			}
 		});
-		btnNewButton.setBounds(38, 282, 89, 31);
+		btnNewButton.setBounds(38, 398, 89, 31);
 		contentPane.add(btnNewButton);
 		
       //Update 	
@@ -213,16 +248,23 @@ public class AddCrops extends JFrame {
 		btnUpdate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				int i = table.getSelectedRow();
-				model.setValueAt(CropTextfield.getText(), i ,0);
-				model.setValueAt(comboBoxP.getSelectedItem(), i ,1);
-				model.setValueAt(FarmerR.getText(), i ,2);
-				model.setValueAt(MarketB.getText(), i ,3);
+				
+				model.setValueAt(id.getText(), i ,0);
+				model.setValueAt(name.getText(), i ,1);
+				model.setValueAt(noofbox.getText(), i ,2);
+				model.setValueAt(crop.getText(), i ,3);
+				model.setValueAt(Quantity.getText(), i ,4);
+				model.setValueAt(comboBoxP.getSelectedItem(), i ,5);
+				model.setValueAt(FarmerR.getText(), i ,6);
+				model.setValueAt(MarketB.getText(), i ,7);
 				JOptionPane.showMessageDialog(null,"Updated");		
 				
 			}
 		});
-		btnUpdate.setBounds(151, 282, 89, 31);
+		btnUpdate.setBounds(151, 398, 89, 31);
 		contentPane.add(btnUpdate);
+		
+		//delete 
 		
 		JButton btnDelete = new JButton("Delete");
 		btnDelete.setFont(new Font("Arial", Font.BOLD, 14));
@@ -233,7 +275,7 @@ public class AddCrops extends JFrame {
 				JOptionPane.showMessageDialog(null,"Deleted");
 			}
 		});
-		btnDelete.setBounds(774, 283, 89, 32);
+		btnDelete.setBounds(771, 397, 89, 32);
 		contentPane.add(btnDelete);
 		
 		
@@ -243,14 +285,18 @@ public class AddCrops extends JFrame {
 		clearb.setFont(new Font("Arial", Font.BOLD, 14));
 		clearb.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				CropTextfield.setText("");
+				id.setText("");
+				name.setText("");
+				noofbox.setText("");
+				crop.setText("");
+				Quantity.setText("");
 				comboBoxP.setSelectedItem("");
 				FarmerR.setText("");
 				MarketB.setText("");
 				JOptionPane.showMessageDialog(null,"Cleared");		
 			}
 		});
-		clearb.setBounds(668, 283, 89, 31);
+		clearb.setBounds(672, 398, 89, 31);
 		contentPane.add(clearb);
 		
 		// back button
@@ -288,13 +334,13 @@ public class AddCrops extends JFrame {
 		contentPane.add(lblNewLabel_1);
 		
 		JTextPane textPane = new JTextPane();
-		textPane.setBounds(892, 199, 469, 454);
+		textPane.setBounds(892, 199, 469, 572);
 		contentPane.add(textPane);
 		
-		JLabel lblNewLabel = new JLabel("Add crops");
+		JLabel lblNewLabel = new JLabel("Farmer Details");
 		lblNewLabel.setForeground(Color.WHITE);
 		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 18));
-		lblNewLabel.setBounds(38, 139, 128, 31);
+		lblNewLabel.setBounds(38, 139, 175, 31);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblPrintData = new JLabel("Print data");
@@ -302,6 +348,67 @@ public class AddCrops extends JFrame {
 		lblPrintData.setFont(new Font("Arial", Font.BOLD, 18));
 		lblPrintData.setBounds(892, 139, 128, 31);
 		contentPane.add(lblPrintData);
+		
+		crop = new JTextField();
+		crop.setFont(new Font("Arial", Font.PLAIN, 16));
+		crop.setColumns(10);
+		crop.setBounds(38, 346, 175, 29);
+		contentPane.add(crop);
+		
+		JLabel lblNewLabel_2 = new JLabel("Add crops");
+		lblNewLabel_2.setForeground(Color.WHITE);
+		lblNewLabel_2.setFont(new Font("Arial", Font.BOLD, 18));
+		lblNewLabel_2.setBounds(38, 270, 128, 31);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblCrops_1_1 = new JLabel("Name");
+		lblCrops_1_1.setForeground(Color.WHITE);
+		lblCrops_1_1.setFont(new Font("Calibri", Font.PLAIN, 20));
+		lblCrops_1_1.setBounds(253, 180, 71, 25);
+		contentPane.add(lblCrops_1_1);
+		
+		Quantity = new JTextField();
+		Quantity.setFont(new Font("Arial", Font.PLAIN, 16));
+		Quantity.setColumns(10);
+		Quantity.setBounds(253, 345, 175, 31);
+		contentPane.add(Quantity);
+		
+		JLabel lblCrops_2 = new JLabel("Production");
+		lblCrops_2.setForeground(Color.WHITE);
+		lblCrops_2.setFont(new Font("Calibri", Font.PLAIN, 20));
+		lblCrops_2.setBounds(473, 311, 144, 25);
+		contentPane.add(lblCrops_2);
+		
+		id = new JTextField();
+		id.setFont(new Font("Arial", Font.PLAIN, 16));
+		id.setColumns(10);
+		id.setBounds(38, 219, 175, 29);
+//		id.setText("1533"+first);
+		contentPane.add(id);
+		
+		JLabel lblCrops_1_1_1 = new JLabel("ID");
+		lblCrops_1_1_1.setForeground(Color.WHITE);
+		lblCrops_1_1_1.setFont(new Font("Calibri", Font.PLAIN, 20));
+		lblCrops_1_1_1.setBounds(38, 185, 71, 25);
+		contentPane.add(lblCrops_1_1_1);
+		
+		noofbox = new JTextField();
+		noofbox.setFont(new Font("Arial", Font.PLAIN, 16));
+		noofbox.setColumns(10);
+		noofbox.setBounds(475, 217, 175, 31);
+		contentPane.add(noofbox);
+		
+		JLabel lblNoOfFamily = new JLabel("No. of Family");
+		lblNoOfFamily.setForeground(Color.WHITE);
+		lblNoOfFamily.setFont(new Font("Calibri", Font.PLAIN, 20));
+		lblNoOfFamily.setBounds(473, 180, 144, 25);
+		contentPane.add(lblNoOfFamily);
+		
+		JLabel mainlogo = new JLabel("");
+		mainlogo.setHorizontalAlignment(SwingConstants.CENTER);
+		mainlogo.setBounds(62, 17, 65, 58);
+		contentPane.add(mainlogo);
+		mainlogo.setIcon(new ImageIcon(logo));
 		
 		
 		
