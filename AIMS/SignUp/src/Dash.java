528e75fc820d2d1513adc8501f640882c8cf653c
Ajitkhadka123 <101698296+Ajitkhadka123@users.noreply.github.com>
2022-05-09 15:48:03 +0545

    Update Dash.java

--- AIMS/SignUp/src/Dash.java
+++ AIMS/SignUp/src/Dash.java
@@ -11,6 +11,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -293,7 +294,8 @@ public class Dash extends JFrame {
 	               
 	    		}
 	    		catch(Exception E){
-	    			E.printStackTrace();
+//	    			E.printStackTrace();
+	    			JOptionPane.showMessageDialog(null, "Data are not shared yet wait for some time and try again");
 	    		}
 	    	}
 	    });
