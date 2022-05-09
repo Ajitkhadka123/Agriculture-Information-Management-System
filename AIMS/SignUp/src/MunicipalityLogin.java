ae3e23d78b163a03751e2e14bacff98e91f67a68
Ajitkhadka123 <101698296+Ajitkhadka123@users.noreply.github.com>
2022-05-09 15:40:23 +0545

    Update MunicipalityLogin.java

--- AIMS/SignUp/src/MunicipalityLogin.java
+++ AIMS/SignUp/src/MunicipalityLogin.java
@@ -1,3 +1,4 @@
+package agriculture;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Image;
@@ -8,6 +9,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
@@ -16,17 +18,26 @@ import javax.swing.Box;
 import javax.swing.border.LineBorder;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import javax.swing.JTextField;
+import javax.swing.JPasswordField;
 
 public class MunicipalityLogin extends JFrame {
 
-	private Image img_login = new ImageIcon(Login.class.getResource("/img/LoginPhoto.png")).getImage().getScaledInstance(350,300, Image.SCALE_SMOOTH);
+	private Image img_login = new ImageIcon(Login.class.getResource("/LoginPhoto.png")).getImage().getScaledInstance(350,300, Image.SCALE_SMOOTH);
     
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField usernametextField;
 	private JTextField Back;
+
+
+	private JButton AdminLoginB;
+	Connection connect = null;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -35,7 +46,7 @@ public class MunicipalityLogin extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Login frame = new Login();
+					MunicipalityLogin frame = new MunicipalityLogin();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -49,7 +60,7 @@ public class MunicipalityLogin extends JFrame {
 	 */
 	public MunicipalityLogin() {
 		setResizable(false);	
-		
+		connect = mysqlconnection.dbConnector();
 		
 		
 		//exit
@@ -62,7 +73,48 @@ public class MunicipalityLogin extends JFrame {
 		contentPane.setLayout(null);
 		
 		//guest button
-		JButton AdminLoginB = new JButton("Login");
+		AdminLoginB = new JButton("Login");
+		AdminLoginB.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String username, password;
+				username=usernametextField.getText();
+				password=passwordField.getText();
+				try {
+					String query = "select * from user where Username=? and Password=?";
+					PreparedStatement state = connect.prepareStatement(query);
+					state.setString(1, username);
+					state.setString(2, password);
+					
+
+					ResultSet result = state.executeQuery();
+					int count = 0;
+					while (result.next()) {
+						count++;
+
+					}
+					if (count == 1) {
+						MunicipalityLogin.this.setVisible(false);
+						Dash door = new Dash(username);
+						door.setVisible(true);
+						
+					} else if (count > 1) {
+						JOptionPane.showMessageDialog(null, "Duplicate UserName or Password");
+						passwordField.setText("");
+						
+					} else {
+						JOptionPane.showMessageDialog(null, "Username or Password is not correct");
+						usernametextField.setText("");
+						passwordField.setText("");
+						
+					}
+					result.close();
+					state.close();
+					
+				} catch (Exception e2) {
+					JOptionPane.showMessageDialog(contentPane,e2);
+				}
+			}
+		});
 		AdminLoginB.addMouseListener(new MouseAdapter() {
 			
 			public void mouseEntered(MouseEvent e) {
@@ -74,60 +126,23 @@ public class MunicipalityLogin extends JFrame {
 				AdminLoginB.setForeground(Color.BLACK);
 			}
 			
-			public void mouseClicked(MouseEvent e) {
-//				AdminLogin.this.dispose();
-//				AdminSection Adashboard = new AdminSection();
-//				Adashboard.setVisible(true);
-//				if(e.getSource()==AdminLoginB) {
-//					try {
-//						new AdminSection().setVisible(true);
-//					}catch (Exception E) {
-//						System.out.println();
-//					}
-//				}
-//			}
-		}});
-		AdminLoginB.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				    MunicipalityLogin.this.dispose();
-					Dash user = new Dash();
-					user.setVisible(true);
-					}
-			
-				
-		}
-				
-			
-		);
-		AdminLoginB.setFont(new Font("Arial", Font.BOLD, 14));
-		AdminLoginB.setBounds(60, 333, 89, 31);
-		contentPane.add(AdminLoginB);
+//			public void mouseClicked(MouseEvent e) {
+////				AdminLogin.this.dispose();
+////				AdminSection Adashboard = new AdminSection();
+////				Adashboard.setVisible(true);
+////				if(e.getSource()==AdminLoginB) {
+////					try {
+////						new AdminSection().setVisible(true);
+////					}catch (Exception E) {
+////						System.out.println();
+////					}
+////				}
+////			}
+			});
 		
-		//reset password
-		JButton AdminLoginBut = new JButton("Reset");
-		AdminLoginBut.addMouseListener(new MouseAdapter() {
-			public void mouseEntered(MouseEvent e) {
-				AdminLoginBut.setForeground(Color.RED);
-				
-			}
-	
-			public void mouseExited(MouseEvent e) {
-				AdminLoginBut.setForeground(Color.BLACK);
-			}
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				MunicipalityLogin.this.dispose();
-				ForgotPassword ResetPass = new ForgotPassword();
-				ResetPass.setVisible(true);
-			}
-		});
-		AdminLoginBut.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		AdminLoginBut.setFont(new Font("Arial", Font.BOLD, 14));
-		AdminLoginBut.setBounds(173, 333, 89, 31);
-		contentPane.add(AdminLoginBut);
+		AdminLoginB.setFont(new Font("Arial", Font.BOLD, 14));
+		AdminLoginB.setBounds(120, 335, 89, 31);
+		contentPane.add(AdminLoginB);
 		
 		//image
 	    JLabel IbllconLogo = new JLabel("");
@@ -149,12 +164,6 @@ public class MunicipalityLogin extends JFrame {
 		verticalBox.setBounds(0, 76, 686, 3);
 		contentPane.add(verticalBox);
 		
-		textField = new JTextField();
-		textField.setFont(new Font("Arial", Font.PLAIN, 16));
-		textField.setColumns(10);
-		textField.setBounds(50, 281, 224, 31);
-		contentPane.add(textField);
-		
 		JLabel lblCrops_1 = new JLabel("Username or Email");
 		lblCrops_1.setForeground(Color.WHITE);
 		lblCrops_1.setFont(new Font("Calibri", Font.PLAIN, 20));
@@ -167,11 +176,11 @@ public class MunicipalityLogin extends JFrame {
 		lblCrops_1_1.setBounds(50, 246, 215, 25);
 		contentPane.add(lblCrops_1_1);
 		
-		textField_1 = new JTextField();
-		textField_1.setFont(new Font("Arial", Font.PLAIN, 16));
-		textField_1.setColumns(10);
-		textField_1.setBounds(50, 192, 224, 31);
-		contentPane.add(textField_1);
+		usernametextField = new JTextField();
+		usernametextField.setFont(new Font("Arial", Font.PLAIN, 16));
+		usernametextField.setColumns(10);
+		usernametextField.setBounds(50, 192, 224, 31);
+		contentPane.add(usernametextField);
 		
 		JLabel lblNewLabel_1 = new JLabel("Agriculture Information Management System");
 		lblNewLabel_1.setForeground(Color.WHITE);
@@ -203,6 +212,9 @@ public class MunicipalityLogin extends JFrame {
 		Back.setBounds(10, 26, 42, 19);
 		contentPane.add(Back);
 		Back.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(50, 281, 224, 31);
+		contentPane.add(passwordField);
 	}
 }
-	
